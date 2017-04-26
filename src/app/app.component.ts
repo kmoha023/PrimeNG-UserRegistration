2699e23fd8d830cb14992f8a72b14d850fd33434
Kmoha023 <kmoha023@fiu.edu>
2017-04-26 15:35:36 -0500

    Commit_1.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
+//The moduleId property is set to allow a relative path to be used for the templateUrl.
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
-export class AppComponent {
-  title = 'Course Registration';
-}
+
+export class AppComponent {  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,58 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { InputTextModule } from 'primeng/primeng';
+import { HttpModule, BaseRequestOptions } from '@angular/http';
 
+//Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AlertComponent } from './directives/alert.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+//Guard
+import { AuthGuard } from './guard/auth.guard';
+
+/*used to create fake backend--
+This is where the fake backend provider is added to the application, to switch to a real backend simply 
+remove the providers located under the comment "// providers used to create fake backend".*/
+import { fakeBackendProvider } from './fakebackend/fake-backend';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
+//Routing module
+import { routing } from './app.routing';
+ 
+//Services module
+import { AlertService } from './services/alert.service';
+import { AuthenticationService } from './services/authentication.service';
+import { UserService } from './services/user.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    InputTextModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+ 
+        // providers used to create fake backend
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
+    ],
+    bootstrap: [AppComponent]
 })
+ 
 export class AppModule { }
